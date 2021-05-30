bda19babfff8aa1c7f09617097b7d7729caf8e29
Kiran9684 <kirankumarbs1034@gmail.com>
2021-05-30 13:16:50 +0530

    Added Pending model classes

---
+++ Implementation/PreLearningBackend/PreLearningBackend/Models/User/CampusMindRegister.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreLearningBackend.Models.User
+{
+    public class CampusMindRegister
+    {
+        public Mind Mind { get; set; }
+        public CampusMind CampusMind { get; set; }
+    }
+}
---
+++ Implementation/PreLearningBackend/PreLearningBackend/Models/User/MindTreeMindRegister.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreLearningBackend.Models.User
+{
+    public class MindTreeMindRegister
+    {
+        public Mind Mind { get; set; }
+
+        public MindTreeMind MindTreeMind { get; set; }
+    }
+}
