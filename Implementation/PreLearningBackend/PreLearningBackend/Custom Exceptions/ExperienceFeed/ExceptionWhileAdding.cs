f02c208fe3236927280d42ebaeb96a487ce94ba8
Suraj Kumar Lochar <locharsurajkumar02@gmail.com>
2021-05-31 10:10:56 +0530

    Added Cotroller and Custom Exceptions to ExperienceFeed

---
+++ Implementation/PreLearningBackend/PreLearningBackend/Custom Exceptions/ExperienceFeed/ExceptionWhileAdding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PreLearningBackend.Controllers
+{
+    [Serializable]
+    internal class ExceptionWhileAdding : Exception
+    {
+        public ExceptionWhileAdding()
+        {
+        }
+
+        public string ErrorMessage()
+        {
+            return "Failed to add to the feed";
+        }
+        public ExceptionWhileAdding(string message) : base(message)
+        {
+        }
+
+        public ExceptionWhileAdding(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ExceptionWhileAdding(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
