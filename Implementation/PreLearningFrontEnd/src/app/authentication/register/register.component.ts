f93f9eed0d4d1a7bf420b07e706ceb399084f157
tapan64 <tapanbehera64@gmail.com>
2021-06-02 12:40:44 +0530

    minor change

--- Implementation/PreLearningFrontEnd/src/app/authentication/register/register.component.ts
+++ Implementation/PreLearningFrontEnd/src/app/authentication/register/register.component.ts
@@ -9,46 +9,55 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   constructor() {}
 
-  constructor() { }
-
   campusMind: boolean = false;
   mindTreeMind: boolean = false;
 
   registerForm = new FormGroup({
-    name : new FormControl('',[Validators.required ,Validators.maxLength(50)]),
-    email : new FormControl('',[Validators.email]),
-    contactNo : new FormControl('',[Validators.required ,Validators.maxLength(12)]),
-    password : new FormControl('',[Validators.required ,Validators.maxLength(16)]),
-    gender : new FormControl('',[Validators.required ,Validators.maxLength(8)]),
-    roleId : new FormControl('',[Validators.required]),
-  })
+    name: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    email: new FormControl('', [Validators.email]),
+    contactNo: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(12),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(16),
+    ]),
+    gender: new FormControl('', [Validators.required, Validators.maxLength(8)]),
+    roleId: new FormControl('', [Validators.required]),
+  });
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
   campusMindRole() {
-      this.campusMind = true;
-      this.mindTreeMind = false;
-      this.registerForm.addControl("engineeringBranch",new FormControl('',[Validators.required ,Validators.maxLength(50)]));
-      this.registerForm.removeControl("location");
-      this.registerForm.removeControl("track");
-     
+    this.campusMind = true;
+    this.mindTreeMind = false;
+    this.registerForm.addControl(
+      'engineeringBranch',
+      new FormControl('', [Validators.required, Validators.maxLength(50)])
+    );
+    this.registerForm.removeControl('location');
+    this.registerForm.removeControl('track');
   }
   mindtreeMindRole() {
     this.campusMind = false;
     this.mindTreeMind = true;
-    this.registerForm.addControl("location",new FormControl('',[Validators.required ,Validators.maxLength(50)]));
-    this.registerForm.addControl("track",new FormControl('',[Validators.required ,Validators.maxLength(50)]));
-    this.registerForm.removeControl("engineeringBranch");
-}
+    this.registerForm.addControl(
+      'location',
+      new FormControl('', [Validators.required, Validators.maxLength(50)])
+    );
+    this.registerForm.addControl(
+      'track',
+      new FormControl('', [Validators.required, Validators.maxLength(50)])
+    );
+    this.registerForm.removeControl('engineeringBranch');
+  }
 
-collectData()
-{
-  // console.log(this.registerForm.value);
-  // console.log(this.registerForm.value.roleId)
-  // console.log(typeof(+this.registerForm.value.roleId))
-  // console.log(this.registerForm.value);
-  this.registerForm.value.roleId = +this.registerForm.value.roleId
-  console.log(this.registerForm.value);
-}
+  collectData() {
+    // console.log(this.registerForm.value);
+    // console.log(this.registerForm.value.roleId)
+    // console.log(typeof(+this.registerForm.value.roleId))
+    // console.log(this.registerForm.value);
+    this.registerForm.value.roleId = +this.registerForm.value.roleId;
+    console.log(this.registerForm.value);
+  }
 }
