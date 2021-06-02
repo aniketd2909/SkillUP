b85117fc6f595e8f5fa205265d682e90f3f52a71
Kiran9684 <kirankumarbs1034@gmail.com>
2021-06-02 11:01:13 +0530

    added register html form

--- Implementation/PreLearningFrontEnd/src/app/app-routing.module.ts
+++ Implementation/PreLearningFrontEnd/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [{ path: 'blocker', loadChildren: () => import('./blocker/blocker.module').then(m => m.BlockerModule) },
-{ path: 'experience-feed', loadChildren: () => import('./experience-feed/experience-feed.module').then(m => m.ExperienceFeedModule) }
+{ path: 'experience-feed', loadChildren: () => import('./experience-feed/experience-feed.module').then(m => m.ExperienceFeedModule) },
+{ path: 'authentication', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) }
 ];
 
 @NgModule({
---
+++ Implementation/PreLearningFrontEnd/src/app/authentication/register/register.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  flag : boolean = false;
+   x  :any 
+  ngOnInit(): void {
+  }
+}
