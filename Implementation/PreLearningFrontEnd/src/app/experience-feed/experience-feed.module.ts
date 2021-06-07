9de71eed83d39eab6089c3b7f04a14f4f7d1dc4a
Suraj Kumar Lochar <locharsurajkumar02@gmail.com>
2021-06-07 15:57:09 +0530

    Added Stylings for ExperienceFeed

--- Implementation/PreLearningFrontEnd/src/app/experience-feed/experience-feed.module.ts
+++ Implementation/PreLearningFrontEnd/src/app/experience-feed/experience-feed.module.ts
@@ -9,6 +9,16 @@ import { UpdateExperienceFeedComponent } from './update-experience-feed/update-e
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { MatListModule } from '@angular/material/list';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -17,7 +27,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     ExperienceFeedRoutingModule,
     FormsModule,   
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatTreeModule,
+    MatListModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
   ]
 })
 export class ExperienceFeedModule { }
