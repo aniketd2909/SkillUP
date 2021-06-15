7770c385bf12ef6be52df7b91c424759b4dbe742
Ramya <ramyaharidass20@gmail.com>
2021-06-15 10:32:29 +0530

    Added selecetdUser in Admin

--- Implementation/PreLearningFrontEnd/src/app/admin/add-user-details/add-user-details.component.ts
+++ Implementation/PreLearningFrontEnd/src/app/admin/add-user-details/add-user-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/services/api.service';
@@ -9,27 +10,42 @@ import { ApiService } from 'src/services/api.service';
   styleUrls: ['./add-user-details.component.css']
 })
 export class AddUserDetailsComponent implements OnInit {
-
+  @ViewChild('fileInput') fileInput; 
+  message: string; 
   Form!: FormGroup;
   List: any;
-  constructor(private fb: FormBuilder,private apiService:ApiService,private route: Router) { }
-
+  constructor(private fb: FormBuilder,private apiService:ApiService,private route: Router,private http:HttpClient) { }
+  url = 'https://localhost:44363/api/SelectedUser';
   ngOnInit(): void {
-    this.Form = this.fb.group({
-     file: ['']   
-    });
-    this.OnSubmit
+    this.onFileUpload
   }
-
-  OnSubmit() {
-    this.apiService.post('SelectedUser',this.Form.value).subscribe((response)=>{
-      console.log(response)
-    }, (error) => {console.log(error.error),alert(error.error)});
+  // uploadFile() {  
+  //   let formData = new FormData();  
+  //   formData.append('upload', this.fileInput.nativeElement.files[0])  
+  
+  //   this.UploadExcel(formData).subscribe(result => {  
+  //     this.message = result.toString();  
+      
+  //   });   
+  
+  // }  
+  onFileUpload(event) {
+    
+    const file = event.files[0];
+    //this.progressBar = true;
+    console.log(event)
+    const formData: FormData = new FormData();
+    formData.append('file', file, file.name);
+    this.UploadExcel(formData).subscribe(x => console.log(x),error=>console.log(error));
   }
-
-  resetForm(){
-    this.Form.reset();
-    alert("Added successfully!")
-   
-  }
+  UploadExcel(formData: FormData) {  
+    let headers = new HttpHeaders();  
+  
+    headers.append('Content-Type', 'multipart/form-data');  
+    headers.append('Accept', 'application/json');  
+  
+    const httpOptions = { headers: headers};  
+  
+    return this.http.post(this.url , formData, httpOptions)  
+  }  
 }
